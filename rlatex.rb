990151eb5b9e17da969faf3ca9218bef52df3dee
Edoardo Colombo <warl0ck.usenet@gmail.com>
2013-06-19 19:40:53 +0200

    package added after line

--- rlatex.rb
+++ rlatex.rb
@@ -2,11 +2,14 @@ gem 'trollop'
 gem 'facets'
 
 require 'fileutils'
+require 'tempfile'
 require 'trollop'
 require 'facets/string/titlecase'
 
 VERSION = "1.2"
 SUB_COMMANDS = %w{new}
+TEMP_FILE = "temp"
+PACKAGES_MARKER = "%%% packages (end)"
 HELP = <<-EOS
 This is rlatex #{VERSION}, a ruby command line utility for LaTeX projects scaffolding.
 
@@ -141,6 +144,11 @@ class LatexCreator
     file.puts "\\usepackage{tikz}"
     file.puts "\\usepackage{hyperref}"
     @packages.each { |pkg| file.puts "\\usepackage{#{pkg}}"} unless @packages.nil?
+    file.puts PACKAGES_MARKER
+  end
+
+  def add_package(package)
+    add_line_before PACKAGES_MARKER, "\\usepackage{#{package}}", "main.tex"
   end
 
   def compile(file = "main.tex")
@@ -157,6 +165,21 @@ class LatexCreator
       return true if File.executable? exe
     end
     return false
+  end
+
+  def add_line_before(line_to_be_found, line_to_be_inserted, path)
+    temp_file = Tempfile.new(TEMP_FILE)
+    begin
+      File.readlines(path).each_cons(2) do |line, next_line|
+        temp_file.puts line
+        temp_file.puts line_to_be_inserted if next_line.chomp == line_to_be_found
+      end
+      temp_file.rewind
+      FileUtils.mv(temp_file.path, path)
+    ensure
+      temp_file.close
+      temp_file.unlink
+    end
   end
 
 end
@@ -204,4 +227,7 @@ when "new"
 when "compile"
   file = ARGV.shift
   if file.nil? then creator.compile else creator.compile file end
+when "add-package"
+  package = ARGV.shift
+  creator.add_package package
 end
